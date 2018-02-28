7f694866c35b6ab567611587b9c387817a3105cb
Joe Stephens <cherry.red@live.co.uk>
2018-02-28 19:04:45 +0000

    Completed numbers up to quotient

--- src/numbers.js
+++ src/numbers.js
@@ -1,43 +1,49 @@
 export const add = (a, b) => {
-  // your code here
+  return a + b
 };
 
 export const subtract = (a, b) => {
-  // your code here
+  return a - b
 };
 
 export const multiply = (a, b) => {
-  // your code here
+  return a * b
 };
 
 export const divide = (a, b) => {
-  // your code here
+  return a / b
 };
 
 export const power = (a, b) => {
-  // your code here
+  return Math.pow(a,b)
 };
 
 export const round = (a) => {
   // your code here
+  return Math.round(a)
 };
 
 export const roundUp = (a) => {
   // your code here
+  return Math.ceil(a)
 };
 
 export const roundDown = (a) => {
   // your code here
+  return Math.floor(a)
 };
 
 export const absolute = (a) => {
   // your code here
+  return Math.abs(a)
 };
 
 export const quotient = (a, b) => {
   // your code here
-};
-
-export const remainder = (a, b) => {
-  // your code here
+  var c = a/b
+  if (c < 0){
+    return Math.ceil(c)
+  } if (c >= 0){
+    return Math.floor(c)
+  }
 };
